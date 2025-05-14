a667ea39a55cdc4e774541b2519942e7e9e2e060
eric matutu <ericmatutu125@gmail.com>
2025-05-14 14:09:21 +0300

    configured express - setup of routes - configured the paths - downloaded the packages(dotenv, express, nodemon)

---
+++ app.js
@@ -0,0 +1,41 @@
+/**
+ * 
+*/
+import dotenv from 'dotenv';
+dotenv.config()
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+import express from "express";
+
+
+const app = express();
+app.use(express.json());
+
+// Reconstruct __dirname
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+ 
+
+
+//serve static files from "public" directory
+app.use(express.static(path.join(__dirname, 'public')));
+
+//set the views directory and the view engine
+app.set('views', path.join(__dirname, 'pages'));
+app.set('view engine', 'html');
+
+app.get('/', (req,res)=>{
+    res.status(200).sendFile(path.join(__dirname,'index.html'));
+});
+
+app.get('/', (req, res)=>{
+    res.status(200).sendFile(path.join(__dirname, 'pages', 'contact.html'));
+})
+
+const PORT = process.env.PORT || 3000;
+
+//  run the server
+app.listen(PORT, () =>{
+    console.log(`server is running on port :${PORT}`);
+})
